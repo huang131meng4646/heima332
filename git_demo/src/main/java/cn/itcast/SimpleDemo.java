cabb15a3e2724457a65edfbc4399178cf62d2602
HuangMeng <1435260706@qq.com>
2020-09-04 17:53:48 +0800

    小a又又又来开发了

--- git_demo/src/main/java/cn/itcast/SimpleDemo.java
+++ git_demo/src/main/java/cn/itcast/SimpleDemo.java
@@ -6,5 +6,6 @@ public class SimpleDemo {
         System.out.println ("小b来开发.." );
         System.out.println ("小a来开发.." );
         System.out.println ("小a又又又来开发.." );
+        System.out.println ("小a又又又来开发了.." );
     }
 }
