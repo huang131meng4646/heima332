3ec3fb011ecabfed0d58da0ecac5bd05e47dde76
HuangMeng <1435260706@qq.com>
2020-09-04 17:42:46 +0800

    小a又来开发了

--- git_demo/src/main/java/cn/itcast/SimpleDemo.java
+++ git_demo/src/main/java/cn/itcast/SimpleDemo.java
@@ -4,5 +4,6 @@ public class SimpleDemo {
     public static void main(String[] args) {
         System.out.println ("推送成功" );
         System.out.println ("小b来开发.." );
+        System.out.println ("小a来开发.." );
     }
 }
